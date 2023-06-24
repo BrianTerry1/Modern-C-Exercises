a6405d50822d19107b95e4e79768c12694a602c1
BrianTerry1 <56925848+BrianTerry1@users.noreply.github.com>
2023-06-24 19:54:41 +0100

    Establish array size.

--- Challenge9.c
+++ Challenge9.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int prime_factor(int N){
-  return N;
+int print_prime_factors(int N){
+  /*
+  Can't use dynamic array size so instead need to pre-judge size of array.
+  On a basic level, #(prime factors of N) cannot be greater than half of all
+  elements so set arr to have size N/2 if N is even and N+1/2 is N is odd. 
+  
+  May double back on this to check if there is a better upper bound.
+  */
+  if (N % 2 == 0){ //N is even
+    int arr[N/2];
+  } else { //N is odd
+    int arr[(N+1)/2];
+  }
 }
 
 int main() {
-  printf("%d", prime_factor(3));
+
   return 0;
 }
